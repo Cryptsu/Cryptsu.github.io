aed0b95140529e990c6e3f38a9aa98c7c6704cda
Mistsuu <vietanhtran12@gmail.com>
2023-03-13 16:52:34 +0700

    Page for testing components

---
+++ pages/test.tsx
@@ -0,0 +1,16 @@
+import { styled, theme } from "@/lib/styles/stiches.config"
+
+const H1 = styled("p", 
+  {
+    color: "rgb(99, 200, 39)"
+  }
+)
+
+export default function Test() {
+  return (
+    <H1>
+      This is where I test my stuffs.
+    </H1>
+  )
+}
+  
