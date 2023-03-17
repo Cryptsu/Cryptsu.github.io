c37ef1fbcbc1914fa51172962c676dd458570e20
Cryptsu <39164879+Cryptsu@users.noreply.github.com>
2023-03-17 11:04:55 +0000

    Deploying to gh-pages from @ Cryptsu/Cryptsu.github.io@0d9afbb2c19e179d7c668a46869db98914606260 🚀

---
+++ _next/static/chunks/pages/index-4d7e42048ffd4816.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return t(2603)}])},2603:function(n,u,t){"use strict";t.r(u),t.d(u,{default:function(){return _}});var r=t(5893);function _(){return(0,r.jsx)(r.Fragment,{})}}},function(n){n.O(0,[774,888,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
