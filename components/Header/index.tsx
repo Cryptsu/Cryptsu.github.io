9e3476d40d79e85f2d17eacbbaaffa2a41a9dcf3
Mistsuu <vietanhtran12@gmail.com>
2023-03-18 17:06:51 +0700

    Header style

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -5,34 +5,74 @@ import { theme } from "@/lib/styles/stiches.config";
 const Header = () => {
   return (
     <Style style={HeaderStyles}>
+      <Style style={NavLinkGroupStyles}>
+        defadsfasfsdaffdsfsafa
+      </Style>
       <Style style={LogoGroupStyles}>
         <Logo/>      
+      </Style>
+      <Style style={ButtonsGroupStyles}>
+        abc
       </Style>
     </Style>
   )
 }
 
-const LogoGroupStyles = {
-  width: "100%",
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  color: theme.colors.black,
-  paddingTop: 12,
-  paddingBottom: 12,
-}
-
 const HeaderStyles = {
-  width: "100%",
+  display: "grid",
+  gridTemplateAreas: "'navlinks logo buttons'",
+  gridTemplateColumns: "2fr 1fr 2fr",
+  gap: "10px",
+
+  // Padding
+  paddingLeft: 200,
+  paddingRight: 200,
+
+  // Create the divider
   borderBottom: 4,
   borderTop: 0,
   borderLeft: 0,
   borderRight: 0,
   borderStyle: "dashed",
   borderColor: theme.colors.divider,
-  position: "relative",
+
+  // Blur background
   backdropFilter: "blur(4px)",
   backgroundColor: theme.colors.blurBackground,
+}
+
+const NavLinkGroupStyles = {
+  // For grid display
+  gridArea: "navlinks",
+
+  // Others
+  color: theme.colors.primary,
+  display: "flex",
+  justifyContent: "left",
+  alignItems: "center",
+}
+
+const LogoGroupStyles = {
+  // For grid display
+  gridArea: "logo", 
+
+  // Others
+  display: "flex",
+  justifyContent: "center",
+  color: theme.colors.black,
+  paddingTop: 12,
+  paddingBottom: 12,
+}
+
+const ButtonsGroupStyles = {
+  // For grid display
+  gridArea: "buttons",
+
+  // Others
+  color: theme.colors.primary,
+  display: "flex",
+  justifyContent: "right",
+  alignItems: "center",
 }
 
 export default Header;
