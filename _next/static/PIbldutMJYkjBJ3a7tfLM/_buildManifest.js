e2357a27a437267251996b478fe6829bbeb74b58
Cryptsu <39164879+Cryptsu@users.noreply.github.com>
2023-03-18 09:07:12 +0000

    Deploying to gh-pages from @ Cryptsu/Cryptsu.github.io@efca394ba40827ef43279ff3627d974c229ee974 🚀

--- _next/static/PIbldutMJYkjBJ3a7tfLM/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-4d7e42048ffd4816.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],"/test":["static/chunks/pages/test-9d96e6cca9d83890.js"],sortedPages:["/","/_app","/_error","/test"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
