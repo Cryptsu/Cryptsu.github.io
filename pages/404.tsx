547188cc1d8b9fb336c4a106c9b139011ccc919f
Cryptsu <bikebaronfan@gmail.com>
2023-04-21 12:31:05 +0700

    20s

--- pages/404.tsx
+++ pages/404.tsx
@@ -75,7 +75,7 @@ const _404 = ({children, ...otherProps}: _404Props) => {
   useEffect(() => {
     const timeout = setTimeout(() => {
       setIsUserStay(true);
-    }, 1000);
+    }, 20000);
     return () => clearTimeout(timeout);
   }, []);
 
