66de72ee823c81bea64c5aa08d2de2fece348850
Mistsuu <vietanhtran12@gmail.com>
2023-05-11 21:35:27 +0000

    Deploying to gh-pages from @ Cryptsu/Cryptsu.github.io@7189c21b4caad07baedb930b0ff2aebf2ea9b92e 🚀

--- _next/static/chunks/pages/about-36569e97869cd69a.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[521],{9212:function(n,u,t){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about",function(){return t(570)}])},570:function(n,u,t){"use strict";t.r(u);var e=t(5893),_=t(1533);let c=()=>(0,e.jsx)(_.Z,{style:i,children:"hello"}),i={};u.default=c}},function(n){n.O(0,[774,888,179],function(){return n(n.s=9212)}),_N_E=n.O()}]);
