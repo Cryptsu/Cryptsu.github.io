f6399b7a6543bd9442d10d953122ec1fc3a056ea
Cryptsu <bikebaronfan@gmail.com>
2023-05-11 18:43:53 +0700

    It's great and all, but we should keep it realistic

--- components/Layouts/HomeLayout/HomeContent/index.tsx
+++ components/Layouts/HomeLayout/HomeContent/index.tsx
@@ -16,73 +16,22 @@ type HomeContentProps = PropsWithChildren<{
 
 const HomeContent = ({children, posts, ...otherProps}: HomeContentProps) => {
   const [ currentPage, setCurrentPage ] = useState<number | null>(null);
-  const [ scrollXYs, setScrollXYs ] = useState<number[]>([0, 0]);
-  const justResize = useRef<boolean>(false);
-  
-  const onPageResize = () => {
-    if (currentPage !== null) {
-      let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-      let scrollWidth  = document.documentElement.scrollWidth  || document.body.scrollWidth;
-      
-      // For non-Chrome solutions
-      document.documentElement.scrollTo({
-        left: scrollWidth - scrollXYs[0], 
-        top: scrollHeight - scrollXYs[1],
-        behavior: "auto",
-      })
-
-      // For Chrome solutions
-      document.body.scrollTo({
-        left: scrollWidth - scrollXYs[0], 
-        top: scrollHeight - scrollXYs[1],
-        behavior: "auto",
-      })
-
-      // Set scroll-size so that
-      // we don't scroll during
-      // page update
-      justResize.current = true;
-    }
-  }
 
   useLayoutEffect(() => {
-    onPageResize();
-  /* eslint-disable react-hooks/exhaustive-deps */}, [currentPage]);
-
-  // Track scrolling on the website.
-  const onContentScroll = () => {
-    // Please don't scroll if page 
-    // has just been recently resized.
-    if (justResize.current) {
-      justResize.current = false;
-      return;
+    if (currentPage !== null) {
+      // For non-Chrome solutions
+      document.documentElement.scrollTo({
+        top: 0,
+        behavior: "auto",
+      })
+  
+      // For Chrome solutions
+      document.body.scrollTo({
+        top: 0,
+        behavior: "auto",
+      })
     }
-
-    let scrollLeft   = document.documentElement.scrollLeft   || document.body.scrollLeft;
-    let scrollTop    = document.documentElement.scrollTop    || document.body.scrollTop;
-    let scrollHeight = document.documentElement.scrollHeight || document.body.scrollHeight;
-    let scrollWidth  = document.documentElement.scrollWidth  || document.body.scrollWidth;
-
-    // X, Y is set to be the counted from the bottom of page
-    // instead of top.
-    // Modify array value then set to avoid creating
-    // a new array pointer, which triggers re-render.
-    scrollXYs[0] = scrollWidth - scrollLeft;
-    scrollXYs[1] = scrollHeight - scrollTop;
-    setScrollXYs(scrollXYs);
-  }
-
-  useEffect(() => { 
-    const observer = new ResizeObserver(onPageResize);
-    observer.observe(document.body);
-    window.addEventListener('scroll', onContentScroll);
-    return (
-      () => {
-        window.removeEventListener('scroll', onContentScroll)
-        observer.unobserve(document.body);
-      }
-    )
-  })
+  /* eslint-disable react-hooks/exhaustive-deps */}, [currentPage]);
 
   return (
     <>
