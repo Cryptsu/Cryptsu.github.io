6b4678579046c14d28a729dd6b937c5cfb703837
Mistsuu <vietanhtran12@gmail.com>
2023-10-19 11:17:54 +0700

    Throw compile error when using :ytembed not correctly

--- lib/helpers/compile-directive.ts
+++ lib/helpers/compile-directive.ts
@@ -2,9 +2,10 @@ import { visit } from 'unist-util-visit';
 import { h } from 'hastscript';
 import { HtmlConst } from "@/lib/consts";
 import type { Root } from "mdast";
+import type { VFile } from "vfile";
 
 export function customRemarkDirective() {
-  return (tree: Root) => {
+  return (tree: Root, file: VFile) => {
     visit(tree, (node) => {
       if (
         node.type === 'textDirective' ||
@@ -12,13 +13,31 @@ export function customRemarkDirective() {
         node.type === 'containerDirective'
       ) 
       {
-        const data = node.data || (node.data = {});
-        const hast = h(node.name, node.attributes === null ? undefined : node.attributes);
-        switch(node.name) {
-          default:
-            data.hName = hast.tagName;
-            data.hProperties = hast.properties;
-            break
+        if (node.name === 'ytembed') {
+          const data = node.data || (node.data = {});
+          const attributes = node.attributes || {};
+          const id = attributes.id;
+
+          if (
+            node.type === 'textDirective' ||
+            node.type === 'containerDirective'
+          ) {
+            file.fail(
+              'Unexpected :ytembed text directive or :::ytembed container directive, please use ::ytembed for a leaf directive.',
+              node
+            )
+          }
+
+          if (!id) {
+            file.fail(
+              'Expect `#id` on `::ytembed` directive: `::ytembed[Description]{#id}`.',
+              node
+            )
+          }
+
+          const hast = h(node.name, attributes === null ? undefined : attributes);
+          data.hName = hast.tagName;
+          data.hProperties = hast.properties;
         }
       }
     })
