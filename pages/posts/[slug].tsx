76dceb238da22d48fd61455302e2891f441e5bc4
Mistsuu <vietanhtran12@gmail.com>
2023-10-25 16:18:31 +0700

    Fix bug where cover image of post is not found when sharing

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,3 +1,6 @@
+import path from "path";
+import { useRouter } from "next/router";
+
 import { NextSeo, ArticleJsonLd } from "next-seo";
 import { articleJsonLd } from "@/lib/config/seo.config"
 
@@ -22,6 +25,30 @@ export type PostProps = PostWithSourceType & {
 }
 
 const Post = ({frontMatter, sourceContent, prevPost, nextPost}: InferGetStaticPropsType<typeof getStaticProps>) => {
+  // If path does not starts with
+  // '/', we can change it into relative
+  // import :) A bit hacky method, though...
+  const router = useRouter();
+  const currentURL = router.asPath.split('/')
+  const possibleSlug = currentURL.pop();
+  const possibleCategory = currentURL.pop();
+  
+  let shareImageURL = frontMatter.coverImage || AppConfig.AUTHOR_LOGO;
+  if (
+    !shareImageURL.startsWith('/') 
+      && !(new RegExp("(^(http|https)://)|(^data:image/)", "i")).test(shareImageURL)
+      && possibleSlug 
+      && possibleCategory
+  ) {
+    shareImageURL = path.join(
+      '/',
+      AppConfig.ATTACH_DIR,
+      possibleCategory,
+      possibleSlug,
+      shareImageURL
+    )
+  }
+
   return (
     <>
       <NextSeo
@@ -40,7 +67,7 @@ const Post = ({frontMatter, sourceContent, prevPost, nextPost}: InferGetStaticPr
           },
           images: [
             {
-              url: `${AppConfig.BLOG_URL}${frontMatter.coverImage || AppConfig.AUTHOR_LOGO}`,
+              url: `${AppConfig.BLOG_URL}/${shareImageURL}`,
               alt: frontMatter.title,
             },
           ],
@@ -56,7 +83,7 @@ const Post = ({frontMatter, sourceContent, prevPost, nextPost}: InferGetStaticPr
         description={frontMatter.description || AppConfig.DESCRIPTIONS.AUTHOR}
         datePublished={frontMatter.date}
         dateModified={frontMatter.date}
-        images={[`${AppConfig.BLOG_URL}${frontMatter.coverImage || AppConfig.AUTHOR_LOGO}`]}
+        images={[`${AppConfig.BLOG_URL}/${shareImageURL}`]}
         {...articleJsonLd}
       />
 
